ea554309e52313695ec18cd07c6947d9e9851e13
Mitterand <asmitterand@yahoo.fr>
2024-08-18 21:30:42 +0200

    #17- header fertig

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
+import { MenubarModule } from 'primeng/menubar';
+import { HeaderComponent } from './layout/header/header.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ButtonModule] ,
+  imports: [RouterOutlet, ButtonModule,HeaderComponent,MenubarModule] ,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'frontend';
+ 
 }
---
+++ src/app/layout/header/header.component.ts
@@ -0,0 +1,30 @@
+import { Component, } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+import { ButtonModule } from 'primeng/button';
+import { MenubarModule } from 'primeng/menubar';
+
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [MenubarModule,ButtonModule],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css'
+})
+export class HeaderComponent  {
+  public items: MenuItem[] =[
+
+    {
+      label: 'Employee',
+      icon: 'pi pi-home'
+    },
+    {
+      label:  'Company organisation chart',
+      icon: 'pi pi-th-large'
+    }
+  ];
+
+  
+}
+
+
