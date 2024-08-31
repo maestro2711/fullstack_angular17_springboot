61d4001db19395125660f5ccf87f10f4673cc790
Mitterand <asmitterand@yahoo.fr>
2024-08-31 22:36:58 +0200

    Webconfig added

---
+++ Angular_app/Backend/src/main/java/fr/afrogeek/Geekhrconnct/config/WebConfig.java
@@ -0,0 +1,20 @@
+package fr.afrogeek.Geekhrconnct.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig  implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:49208")
+                .allowedMethods("GET","POST","PUT","DELETE")
+                .allowCredentials(true);
+    }
+
+
+}
